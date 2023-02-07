04bbf5b6a277cfa8fc1a04f59ecd4d422cc8c51c
Kazutano <123896380+Kazutano@users.noreply.github.com>
2023-02-07 20:48:24 +0200

    Update hungarianalgo.cpp

--- hungarianalgo.cpp
+++ hungarianalgo.cpp
@@ -110,9 +110,72 @@ int main()
 
     return 0;
 }
+{
+    def updategraph(graph, row, col):
+        for i in range(len(graph)):
+            if graph[i][col] == 0:
+                continue
+            if i != row:
+                ratio = graph[i][col] / graph[row][col]
+                for j in range(len(graph[0])):
+                    graph[i][j] = graph[i][j] - ratio * graph[row][j]
 
+    def markzeros(graph, col):
+        zero_rows = []
+        for i in range(len(graph)):
+            if graph[i][col] == 0:
+                zero_rows.append(i)
+        return zero_rows
+
+    def markmorezeros(graph, row, col):
+        for i in range(len(graph[0])):
+            if graph[row][i] == 0:
+                continue
+            zeros = markzeros(graph, i)
+            if len(zeros) <= 1:
+                continue
+            for zero in zeros:
+                if zero == row:
+                    continue
+                ratio = graph[zero][i] / graph[row][i]
+                for j in range(len(graph[0])):
+                    graph[zero][j] = graph[zero][j] - ratio * graph[row][j]
+
+    def find_smallest_column(graph):
+        for i in range(len(graph[0])):
+            non_zero_rows = []
+            for j in range(len(graph)):
+                if graph[j][i] != 0:
+                    non_zero_rows.append(j)
+            if len(non_zero_rows) > 0:
+                return i
+        return -1
+
+    def echelon(graph):
+        i = 0
+        j = 0
+        while i < len(graph) and j < len(graph[0]):
+            non_zero_rows = []
+            for k in range(i, len(graph)):
+                if graph[k][j] != 0:
+                    non_zero_rows.append(k)
+            if len(non_zero_rows) == 0:
+                j = j + 1
+                continue
+            else:
+                row = non_zero_rows[0]
+                for k in range(j, len(graph[0])):
+                    graph[row][k], graph[i][k] = graph[i][k], graph[row][k]
+                updategraph(graph, i, j)
+                markmorezeros(graph, i, j)
+            i = i + 1
+            j = j + 1
+        return graph
+    }
 void HungarianAlgorithm::step5()
 {
+    
+    
     int k = 0;
     for (int i = 1; i <= n; i++)
     {
@@ -161,3 +224,4 @@ void HungarianAlgorithm::step5()
         }
     }
 }
+
